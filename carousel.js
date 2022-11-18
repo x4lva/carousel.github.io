bdd42b89ab108ef04d365c4097c6cf7bc81b362d
Dmytro Khevpa <dmytro.khevpa@kobizo.de>
2022-11-18 16:15:59 +0200

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.dei-pager-carousel-item-wrapper');
+    const bg = document.querySelector('#slider-container .dei-pager-carousel-container-image');
+
+    Array.from(items).forEach(el => {
+        el.addEventListener('mouseenter', (e) => {
+            if (bg.dataset.image === el.dataset.image) return;
+
+            bg.style.opacity = 0;
+            bg.style.backgroundImage = `url('assets/${el.dataset.image}')`;
+            bg.dataset.image = el.dataset.image;
+            setTimeout(() => {
+                bg.style.opacity = 1;
+            }, 200)
+        })
+    })
+})
